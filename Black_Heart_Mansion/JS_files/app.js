0c78e1f7c011376295ad64a9bdd3912a00447f12
Nykarri <karri.bonez@gmail.com>
2020-08-03 18:49:16 -0400

    fixed audio functionality on gmae home page

--- Black_Heart_Mansion/JS_files/app.js
+++ Black_Heart_Mansion/JS_files/app.js
@@ -1,4 +1,36 @@
-console.log ($);
-$(document).on('click', (event) => {
-    $("#audio").get(0).play();
+// console.log ($); << test Jquery and JS connectivity
+
+
+$(() => {
+
+    //================================================
+    // ========Game Home Page Audio Controls==========
+    //================================================
+
+    // $(document).on('click', (event) => {
+    //     $("#audio").get(0).play();
+    // });
+
+    //====Updated audio functionality >>toggles mute/unmute sound and button=======
+
+    const button = $('#button');
+    const icon = $('#button > i');
+    const audio = $('#audio');
+
+    button.on('click', () => {
+        if (audio.paused) {
+            audio.volume = 0.2;
+            audio.play();
+            icon.removeClass('fa-volume-up');
+            icon.addClass('fa-volume-mute');
+
+        } else {
+            audio.pause();
+            icon.removeClass('fa-volume-mute');
+            icon.addClass('fa-volume-up');
+        }
+        button.addClass('fade');
+    });
+
 });
+
