7553d5709742d5005693d7bcfbda00fe332a99bd
Nykarri <karri.bonez@gmail.com>
2020-08-03 19:52:10 -0400

    fixed audio functionality fr this time!!

--- Black_Heart_Mansion/JS_files/app.js
+++ Black_Heart_Mansion/JS_files/app.js
@@ -13,23 +13,12 @@ $(() => {
 
     //====Updated audio functionality >>toggles mute/unmute sound and button=======
 
-    const button = $('#button');
-    const icon = $('#button > i');
-    const audio = $('#audio');
+    $('#unmute').on('click', (event) => {
+        $('#audio').get(0).play();
+    });
 
-    button.on('click', () => {
-        if (audio.paused) {
-            audio.volume = 0.2;
-            audio.play();
-            icon.removeClass('fa-volume-up');
-            icon.addClass('fa-volume-mute');
-
-        } else {
-            audio.pause();
-            icon.removeClass('fa-volume-mute');
-            icon.addClass('fa-volume-up');
-        }
-        button.addClass('fade');
+    $('#mute').on('click', (event) => {
+        $('#audio').get(0).pause();
     });
 
 });
