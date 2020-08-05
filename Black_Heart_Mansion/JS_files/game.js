0b08b9dc12ef943e2a9848b0bb5dec1afc6dd536
Nykarri <karri.bonez@gmail.com>
2020-08-05 00:17:01 -0400

    added How to modal

---
+++ Black_Heart_Mansion/JS_files/game.js
@@ -0,0 +1,27 @@
+console.log($)
+
+$(() => {
+
+    //================================================
+    // ==========Game Instructions Box==========
+    //================================================
+
+    const $openBtn = $('#openModal');
+    const $modal = $('#modal');
+    const $closeBtn = $('#close');
+
+
+    const openModal = () => {
+        $modal.css('display', 'block');
+    }
+
+    const closeModal = () => {
+        $modal.css('display', 'none');
+    }
+
+    $openBtn.on('click', openModal);
+    $closeBtn.on('click', closeModal);
+
+    setTimeout(openModal, 3000);
+
+});
