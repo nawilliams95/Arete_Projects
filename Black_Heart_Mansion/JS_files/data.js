4f63515c3d4461cea950f38b6c8e5256b32b132d
Nykarri <karri.bonez@gmail.com>
2020-08-05 20:37:09 -0400

    added dice rolling function

--- Black_Heart_Mansion/JS_files/data.js
+++ Black_Heart_Mansion/JS_files/data.js
@@ -1,1 +1,40 @@
-console.log($)
+// console.log($)
+
+$(() => {
+
+    //================================================
+    // =============Game player set up================
+    //================================================
+
+
+
+
+
+    //================================================
+    // ================Dice Setup====================
+    //================================================
+
+    const die = $('.dice');
+
+    const rollDice = () => {
+        console.log(Math.floor(Math.random() * 6) + 1);
+
+    }
+    die.on('mousedown', (event) => {
+        die.draggable();
+    });
+
+    die.on('mouseup', rollDice);
+
+    //================================================
+    // ========Game point/space system set up=========
+    //================================================
+    const oneSpace = $('.onePoint');
+    const twoSpace = $('.twoPoints');
+    const threeSpace = $('.threePoints');
+    const penaltySpace = $('.pointDeduction');
+    const challengeSpace = $('.challenge');
+    const startSpace = $('.start');
+
+
+});
